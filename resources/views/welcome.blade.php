de25c1778f87b67c1057a17475ec8c30c78a9099
Pritika-01 <preetika@ambition.guru>
2024-01-31 19:01:49 +0545

    expense tracker

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Expense Tracker</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <header>
+        <div class="lg:container mx-auto ">
+            <div class="flex justify-between">
+                <p>hey</p>
+                <p>hey</p>
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
